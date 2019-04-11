dfa90c4451b80958fae0923fc8833f1d85244e2f
段体华 <duantihua@gmail.com>
2019-04-11 10:10:04 +0800

    format code

--- webapp/src/main/scala/org/openurp/edu/llc/web/action/teacher/TeacherLectureAction.scala
+++ webapp/src/main/scala/org/openurp/edu/llc/web/action/teacher/TeacherLectureAction.scala
@@ -1,3 +1,21 @@
+/*
+ * OpenURP, Agile University Resource Planning Solution.
+ *
+ * Copyright © 2014, The OpenURP Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openurp.edu.llc.web.action.teacher
 
 import org.beangle.webmvc.entity.action.RestfulAction
@@ -49,8 +67,8 @@ class TeacherLectureAction extends RestfulAction[Lecture] {
     populateConditions(builder)
     builder.orderBy(get(Order.OrderStr).orNull).limit(getPageLimit)
   }
-  
-  
+
+
   def audiences(): View = {
     val lectureId = longId("lecture")
     val lecture = entityDao.get(classOf[Lecture], lectureId)
@@ -58,6 +76,5 @@ class TeacherLectureAction extends RestfulAction[Lecture] {
     put("audiences", lecture.audiences)
     forward()
   }
-
 
 }
